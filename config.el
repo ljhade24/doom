583d8bf325643914fc3c5e637122f11cad7b2d3a
이형주 <ljhade24@emacser-mac.local>
2024-12-09 00:12:53 +0900

    font size

--- config.el
+++ config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "D2CodingLigature Nerd Font" :size 16)
-      doom-variable-pitch-font (font-spec :family "D2CodingLigature Nerd Font Propo" :size 16))
+(setq doom-font (font-spec :family "D2CodingLigature Nerd Font" :size 14)
+      doom-variable-pitch-font (font-spec :family "D2CodingLigature Nerd Font Propo" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
